c97941d2f764ec66771e5e01a0892a4c1ebc6299
kawarimidoll <kawarimidoll+git@gmail.com>
2021-05-24 17:23:47 +0900

    add class Question

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -30,20 +31,15 @@ class _QuizPageState extends State<QuizPage> {
   Icon ngIcon = Icon(Icons.close, color: Colors.red);
 
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
-  List<bool> answers = [
-    false,
-    true,
-    true,
+  List<Question> questions = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a:false),
+    Question(q: 'Approximately one quarter of human bones are in the feet.', a:true),
+    Question(q: 'A slug\'s blood is green.', a:true),
   ];
   int questionIndex = 0;
 
   void confirmAnswer(bool userAnswer) {
-    scoreKeeper.add(answers[questionIndex] == userAnswer ? okIcon : ngIcon);
+    scoreKeeper.add(questions[questionIndex].questionAnswer == userAnswer ? okIcon : ngIcon);
   }
 
   void nextQuestion() {
@@ -64,7 +60,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionIndex],
+                questions[questionIndex].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
