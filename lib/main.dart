6b1ba690bfc90b6f531afde95cb7a36911332cfb
kawarimidoll <kawarimidoll+git@gmail.com>
2021-05-24 17:35:04 +0900

    add class QuizBrain

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -31,19 +33,17 @@ class _QuizPageState extends State<QuizPage> {
   Icon ngIcon = Icon(Icons.close, color: Colors.red);
 
   List<Icon> scoreKeeper = [];
-  List<Question> questions = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a:false),
-    Question(q: 'Approximately one quarter of human bones are in the feet.', a:true),
-    Question(q: 'A slug\'s blood is green.', a:true),
-  ];
   int questionIndex = 0;
 
   void confirmAnswer(bool userAnswer) {
-    scoreKeeper.add(questions[questionIndex].questionAnswer == userAnswer ? okIcon : ngIcon);
+    scoreKeeper.add(
+        quizBrain.questions[questionIndex].questionAnswer == userAnswer
+            ? okIcon
+            : ngIcon);
   }
 
   void nextQuestion() {
-    if (questionIndex < questions.length - 1) {
+    if (questionIndex < quizBrain.questions.length - 1) {
       questionIndex++;
     }
   }
@@ -60,7 +60,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionIndex].questionText,
+                quizBrain.questions[questionIndex].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
